706ebfcf7b5e6f4ffee4ad1cd69e53e9931e6742
5430 <quasar5430@gmail.com>
2017-08-19 22:41:34 +0900

    최초본

--- src/triangle_area.jl
+++ src/triangle_area.jl
@@ -1,5 +1,18 @@
 module triangle_area
 
 # package code goes here
+export heron
+@doc "
+   헤론의 공식을 이용하여 세 변의 길이를 입력하면 삼각형의 넓이를 구해 주는 함수
 
+   사용법 : n=heron(a,b,c)
+
+   a,b,c : 삼각형 각 변의 길이
+
+   n : 삼각형의 넓이"->
+   function heron(a,b,c)
+   s=(a+b+c)/2
+   return sqrt(s*(s-a)*(s-b)*(s-c))
+   end
+   
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,6 @@ using triangle_area
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+area=heron(3,4,5)
+println("변의 길이가 3, 4, 5인 삼각형의 넓이는 ",area)
+@test area==6
